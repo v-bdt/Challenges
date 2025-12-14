a705f2f48ac23f1186fbda9e426996f262480411
v-bdt <valentinbauduret@yahoo.fr>
2025-12-14 16:47:16 +0100

    Update webshell_lang.php

--- webshell_lang.php
+++ webshell_lang.php
@@ -1,5 +1,25 @@
-<?php 
-shell_exec('id');
-exec('id');
-passthru('id');
+<?php
+$dir    = '.';
+$files1 = scandir($dir);
+$files2 = scandir($dir, SCANDIR_SORT_DESCENDING);
+$files3 = scandir($dir, SCANDIR_SORT_DESCENDING);
+$files4 = scandir($dir, SCANDIR_SORT_DESCENDING);
+$files5 = scandir($dir, SCANDIR_SORT_DESCENDING);
+$files6 = scandir($dir, SCANDIR_SORT_DESCENDING);
+$files7 = scandir($dir, SCANDIR_SORT_DESCENDING);
+$files8 = scandir($dir, SCANDIR_SORT_DESCENDING);
+$files9 = scandir($dir, SCANDIR_SORT_DESCENDING);
+$files10 = scandir($dir, SCANDIR_SORT_DESCENDING);
+
+print_r($files1);
+print_r($files2);
+print_r($files3);
+print_r($files4);
+print_r($files5);
+print_r($files6);
+print_r($files7);
+print_r($files8);
+print_r($files9);
+print_r($files10);
+
 ?>
